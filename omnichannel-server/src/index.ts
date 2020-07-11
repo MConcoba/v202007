59649d7c9603c9af38b091d85eb5f7b2d2062f90
Manuel Concoba <mconcoba20@gmail.com>
2020-07-11 13:15:44 -0600

    Create libraries files

--- omnichannel-server/src/index.ts
+++ omnichannel-server/src/index.ts
@@ -1,15 +1,24 @@
-import { Request, Response } from "express";
 import Dependencies from "@corecodeio/libraries/di";
-import { MessageScourceControllerInjectionKey } from "./feature/menssage-source/controller/MessageScourceControllerInjectionKey";
+import { Request, Response } from "express";
+import { MessageSourceControllerInjectionKey } from "./feature/message-source/MessageSourceControllerInjectionKey";
 import server from "./server";
 
-server.post("/menssage-source", async (req: Request, res: Response) => {
-  console.log(req.body);
-  return res.status(200);
-}); 
-
 const dependencies = new Dependencies();
-const MessageScourceController = dependencies.provide(MessageScourceControllerInjectionKey);
+
+server.post("/message-source", async (req: Request, res: Response) => {
+  const MessageSourceController = dependencies.provide(
+    MessageSourceControllerInjectionKey
+  );
+
+  // recibir la solicitud (payload de twilio o messagebird MessageSourceProvider)
+  // estandarizar la solicitud (MessageSource{Digest|Parser} || MessageFormatter)
+  // guardar la metadata del mensaje en una base de datos
+  // notificar a maría (native client) que recibió un mensaje
+  // PostRequestServer, MVC, MessageFormatter, MessageController
+  // Dependency Injection
+
+  return res.status(200);
+});
 
 server.listen("8001", () => {
   console.log("listening");
